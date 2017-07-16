48920653e454de57e532b4ee9e00119cecde1c51
purplemuskox <anoopa@gmail.com>
2017-07-15 19:16:31 -0700

    misc utils

---
+++ misc/server.sh
@@ -0,0 +1,3 @@
+!/bin/bash
+cd /C/Users/boss/donkey
+docker run -p 8887:8887 -v c:/users/boss/mydonkey:/root/mydonkey donkey
---
+++ misc/startcar.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 ./donkey/scripts/drive.py --remote http://192.168.1.21:8887
